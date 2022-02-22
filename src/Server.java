b3dd27d06b3bb85e0ea6c12c241dcd2c9e1369f1
Ratmir <foxratmir2000@gmail.com>
2022-02-22 17:42:39 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,20 +1,23 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Server {
   public static void main(String[] args) throws IOException
   {
-    try{
-      ServerSocket serverSocket = new ServerSocket(8888);
-      Socket clientSocket = serverSocket.accept();
-      DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
-      DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+    while (true) {
 
 
+      try {
+        ServerSocket serverSocket = new ServerSocket(8888);
+        Socket clientSocket = serverSocket.accept();
+        DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
+        DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-      StringBuilder contentBuilder = new StringBuilder();
+
+      /*StringBuilder contentBuilder = new StringBuilder();
       try {
         BufferedReader in = new BufferedReader(new FileReader("/public/clowns.html"));
         String str;
@@ -24,20 +27,30 @@ public class Server {
         in.close();
       } catch (IOException e) {
       }
+
       String content = contentBuilder.toString();
+      */
+        String html = "<html><head><title>The clown page</title></<head></html>";
+
+        final String CRLF = "\n\r";
+
+        String response =
+                "HTTP/1.1 200 OK" + CRLF +
+                        "Content-Length: " + html.getBytes().length + CRLF +
+                        CRLF +
+                        html +
+                        CRLF + CRLF;
+
+        outputStream.write(response.getBytes());
 
 
-
-
-
-      inputStream.close();
-      outputStream.close();
-      clientSocket.close();
-      serverSocket.close();
+        inputStream.close();
+        outputStream.close();
+        clientSocket.close();
+        serverSocket.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
-    catch(IOException e){
-      e.printStackTrace();
-    }
-
   }
 }
