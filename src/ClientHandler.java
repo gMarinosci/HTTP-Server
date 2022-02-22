4251b9af9e8dc67992ed1a3918d61f556ed670fe
Gabriele Marinosci <gm222hj@student.lnu.se>
2022-02-22 20:11:52 +0100

    Changes

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -1,86 +1,66 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class ClientHandler extends Thread {
-  DateFormat fordate = new SimpleDateFormat("yyyy/MM/dd");
-  DateFormat fortime = new SimpleDateFormat("hh:mm:ss");
-  final DataInputStream dis;
-  final DataOutputStream dos;
-  final Socket s;
+public class ClientHandler implements Runnable {
 
+  private Socket socket;
+  private String path;
 
-  // Constructor
-  public ClientHandler(Socket s, DataInputStream dis, DataOutputStream dos)
-  {
-    this.s = s;
-    this.dis = dis;
-    this.dos = dos;
+  public ClientHandler (Socket socket) {
+    this.socket = socket;
+    this.path = "/Users/gabrielemarinosci/IdeaProjects/compnet_ass2/public";
   }
 
   @Override
-  public void run()
-  {
-    String received;
-    String toreturn;
-    while (true)
-    {
-      try {
+  public void run() {
 
-        // Ask user what he wants
-        dos.writeUTF("What do you want?[Date | Time]..\n"+
-                "Type Exit to terminate connection.");
+    try {
+      InputStream in = socket.getInputStream();
+      OutputStream out = socket.getOutputStream();
+      List<String> request = readRequest(in);
 
-        // receive the answer from client
-        received = dis.readUTF();
+      if (request.get(0).equals("GET")) {
 
-        if(received.equals("Exit"))
-        {
-          System.out.println("Client " + this.s + " sends exit...");
-          System.out.println("Closing this connection.");
-          this.s.close();
-          System.out.println("Connection closed");
-          break;
+        if (request.get(1).equals("/")) {
+          sendResponse(200, false);
+        } else {
+          if (new File(request.get(1)).exists()) {
+            sendResponse(200, true);
+          } else {
+            sendResponse(404, false);
+          }
         }
-
-        // creating Date object
-        Date date = new Date();
-
-        // write on output stream based on the
-        // answer from the client
-        switch (received) {
-
-          case "Date" :
-            toreturn = fordate.format(date);
-            dos.writeUTF(toreturn);
-            break;
-
-          case "Time" :
-            toreturn = fortime.format(date);
-            dos.writeUTF(toreturn);
-            break;
-
-          default:
-            dos.writeUTF("Invalid input");
-            break;
-        }
-      } catch (IOException e) {
-        e.printStackTrace();
       }
-    }
 
-    try
-    {
-      // closing resources
-      this.dis.close();
-      this.dos.close();
 
-    }catch(IOException e){
+    } catch (IOException e) {
       e.printStackTrace();
+    }
+  }
+
+  public List<String> readRequest(InputStream in) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+    List<String> tokens = new ArrayList<>();
+    StringTokenizer st = new StringTokenizer(reader.readLine());
+
+    while (st.hasMoreElements()) {
+      tokens.add(st.nextToken());
+    }
+    return tokens;
+  }
+
+  public void sendResponse(int httpCode, boolean isFile) {
+
+    if (httpCode == 200) {
+      //TODO 200 response
+    } else if (httpCode == 404) {
+      //TODO implement 404 response
     }
   }
 }
--- src/Server.java
+++ src/Server.java
@@ -5,51 +5,30 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Server {
-  public static void main(String[] args) throws IOException
-  {
-    while (true) {
+  public static void main(String[] args) throws IOException {
 
+    ServerSocket server = null;
 
-      try {
-        ServerSocket serverSocket = new ServerSocket(8888);
-        Socket clientSocket = serverSocket.accept();
-        DataInputStream inputStream = new DataInputStream(clientSocket.getInputStream());
-        DataOutputStream outputStream = new DataOutputStream(clientSocket.getOutputStream());
+    try {
 
+      server = new ServerSocket(8888);
 
-      /*StringBuilder contentBuilder = new StringBuilder();
-      try {
-        BufferedReader in = new BufferedReader(new FileReader("/public/clowns.html"));
-        String str;
-        while ((str = in.readLine()) != null) {
-          contentBuilder.append(str);
-        }
-        in.close();
-      } catch (IOException e) {
+      while (true) {
+
+        Socket socket = server.accept();
+
+        ClientHandler client = new ClientHandler(socket);
+        new Thread(client).start();
       }
-
-      String content = contentBuilder.toString();
-      */
-        String html = "<html><head><title>The clown page</title></<head></html>";
-
-        final String CRLF = "\n\r";
-
-        String response =
-                "HTTP/1.1 200 OK" + CRLF +
-                        "Content-Length: " + html.getBytes().length + CRLF +
-                        CRLF +
-                        html +
-                        CRLF + CRLF;
-
-        outputStream.write(response.getBytes());
-
-
-        inputStream.close();
-        outputStream.close();
-        clientSocket.close();
-        serverSocket.close();
-      } catch (IOException e) {
-        e.printStackTrace();
+    } catch (IOException e){
+      e.printStackTrace();
+    } finally {
+      if (server != null) {
+        try {
+          server.close();
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
